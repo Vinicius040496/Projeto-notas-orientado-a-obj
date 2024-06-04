3132560b0ec42f89bf0674e51b758b8e94223209
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-04 12:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp127/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp127
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double Media()
+        {
+            return Nota1 + Nota2 + Nota3 / 3;
+        }
+
+        public bool Aprovado()
+        {
+            if (Media() >= 15.00)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+    }
+}
---
+++ ConsoleApp127/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp127
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno Al = new Aluno();
+
+            Console.WriteLine("Nome do aluno: ");
+            Al.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            Al.Nota1 = double.Parse(Console.ReadLine());
+            Al.Nota2 = double.Parse(Console.ReadLine());
+            Al.Nota3 = double.Parse(Console.ReadLine());
+            Al.Media();
+            Al.Aprovado();
+            Console.WriteLine("Media do aluno: {0},", Al.Media());
+
+            if (Al.Aprovado())
+            {
+                Console.WriteLine("Parabéns, Aluno Aprovado!!");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, Aluno Reprovado!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
